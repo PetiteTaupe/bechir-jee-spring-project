15e19f0d1c5c0efc15de22adf8cb73894e81369e
Béchir Alexis <alexis.bechir@he-arc.ch>
2026-01-08 14:17:07 +0100

    feat: add BeerRepository interface

---
+++ src/main/java/ch/hearc/jee2025/bechirjeespringproject/beer/BeerRepository.java
@@ -0,0 +1,7 @@
+package ch.hearc.jee2025.bechirjeespringproject.beer;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BeerRepository extends CrudRepository<Beer,Long> {
+
+}
