7ef3a5111dca67a05a5e2dfeef38e249f8ccb907
Béchir Alexis <alexis.bechir@he-arc.ch>
2026-01-08 15:22:51 +0100

    feat: add BeerServiceImpl

--- src/main/java/ch/hearc/jee2025/bechirjeespringproject/beer/BeerServiceImpl.java
+++ src/main/java/ch/hearc/jee2025/bechirjeespringproject/beer/BeerServiceImpl.java
@@ -1,4 +1,38 @@
 package ch.hearc.jee2025.bechirjeespringproject.beer;
 
-public class BeerServiceImpl {
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class BeerServiceImpl implements BeerService {
+
+    public BeerServiceImpl(BeerRepository beerRepository) {
+        this.beerRepository = beerRepository;
+    }
+
+    @Override
+    public Beer save(Beer beer) {
+        return beerRepository.save(beer);
+    }
+
+    @Override
+    public Optional<Beer> findById(Long id) {
+        return beerRepository.findById(id);
+    }
+
+    @Override
+    public Iterable<Beer> findAll() {
+        return beerRepository.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        if (!beerRepository.existsById(id)) {
+            throw new RuntimeException("Beer not found with id: " + id);
+        }
+        beerRepository.deleteById(id);
+    }
+
+    private final BeerRepository beerRepository;
 }
