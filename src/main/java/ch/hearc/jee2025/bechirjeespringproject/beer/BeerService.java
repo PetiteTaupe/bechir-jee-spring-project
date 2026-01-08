d811fd9961126556de8cf448f59bea419e863013
Béchir Alexis <alexis.bechir@he-arc.ch>
2026-01-08 15:22:26 +0100

    feat: add BeerService

--- src/main/java/ch/hearc/jee2025/bechirjeespringproject/beer/BeerService.java
+++ src/main/java/ch/hearc/jee2025/bechirjeespringproject/beer/BeerService.java
@@ -1,4 +1,11 @@
 package ch.hearc.jee2025.bechirjeespringproject.beer;
 
+import java.util.Optional;
+
 public interface BeerService {
+
+    Beer save(Beer beer);
+    Optional<Beer> findById(Long id);
+    Iterable<Beer> findAll();
+    void deleteById(Long id);
 }
